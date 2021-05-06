4381a4404742a158a2db9875c062c71647d2b25d
rachel-tindel <66395861+rachel-tindel@users.noreply.github.com>
2021-05-05 18:37:16 -0600

    Structure backend
    Create the controllers, the data and the constructors.

---
+++ Project_SPA/Project_SPA/Models/Domain/Professor.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project_SPA.Models.Domain
+{
+    public class Professor
+    {
+
+        private int id;
+        private string code;
+        private string name;
+        private string email;
+        private string academic_degree;
+
+        public Professor(int id, string code, string name, string email, string academic_degree)
+        {
+            this.id = id;
+            this.code = code;
+            this.name = name;
+            this.email = email;
+            this.academic_degree = academic_degree;
+        }
+
+        public int Id { get => id; set => id = value; }
+        public string Code { get => code; set => code = value; }
+        public string Name { get => name; set => name = value; }
+        public string Email { get => email; set => email = value; }
+        public string Academic_degree { get => academic_degree; set => academic_degree = value; }
+    }
+}
