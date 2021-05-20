562409edca44b700a049d2e54d9039c553e31d77
Luis Carlos Aguilar Morales <luiscarlos.aguilar@ucr.ac.cr>
2021-05-20 01:16:57 -0600

    Create UserController.cs and AdminDAO.cs

---
+++ Project_SPA/Project_SPA/Models/Data/AdminDAO.cs
@@ -0,0 +1,50 @@
+﻿using Project_SPA.Models.Entities;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Project_SPA.Models.Data
+{
+    public class AdminDAO
+    {
+        
+        private readonly IF4101_2021_SPAContext _context;
+        private List<Admin> admins;
+
+        public AdminDAO(IF4101_2021_SPAContext context)
+        {
+            _context = context;
+        }
+
+        public AdminDAO()
+        {
+        }
+
+        public Entities.Professor GetAdminByCode(string code)
+        {
+            ProfessorDAO professorDAO = new ProfessorDAO();
+
+            if(professorDAO.GetProfessorByCode(code)!= null)
+            {
+                Professor professor = professorDAO.GetProfessorByCode(code);
+
+                if (AdminExists(professor.Id))
+                {
+                    if (professor.Code.Equals(code))
+                    {
+                        return professor;
+                    }
+
+                }
+            }
+
+            
+            return null;
+        }
+
+        private bool AdminExists(int id)
+        {
+            return _context.Admins.Any(e => e.ProfessorId == id);
+        }
+        
+    }
+}
