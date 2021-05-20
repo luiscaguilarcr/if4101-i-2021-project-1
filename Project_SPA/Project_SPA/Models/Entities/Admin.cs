2834fdf88e1bab5352fb4f4afe1092804c8f9498
Luis Carlos Aguilar Morales <luiscarlos.aguilar@ucr.ac.cr>
2021-05-20 01:02:04 -0600

    Add Admin and Temporal Student

---
+++ Project_SPA/Project_SPA/Models/Entities/Admin.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace Project_SPA.Models.Entities
+{
+    public partial class Admin
+    {
+        public int ProfessorId { get; set; }
+
+        public virtual Professor Professor { get; set; }
+    }
+}
