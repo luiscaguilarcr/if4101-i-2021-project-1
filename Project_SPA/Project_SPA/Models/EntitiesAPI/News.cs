78fe6eb99f34f4985997fa7f70b5f5f9b8376fbf
rachel-tindel <66395861+rachel-tindel@users.noreply.github.com>
2021-05-21 13:09:17 -0600

    Create API and database

---
+++ Project_SPA/Project_SPA/Models/EntitiesAPI/News.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace Project_SPA.Models.EntitiesAPI
+{
+    public partial class News
+    {
+        public int Id { get; set; }
+        public string NewsTitle { get; set; }
+        public string Descrip { get; set; }
+        public byte[] Image { get; set; }
+        public byte[] File { get; set; }
+    }
+}
